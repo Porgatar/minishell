aa32d6719d3a6a1b026957caec810190a85669d8
parinder MSI <parinder@student.42angouleme.fr>
2023-11-24 19:22:21 +0100

    moved ft_env_var.c to ft_env.c, exec_builtons to ft_exec_builtins and added ft_pwd.c

--- Srcs/Builtins/ft_exec_builtins.c
+++ Srcs/Builtins/ft_exec_builtins.c
@@ -6,8 +6,27 @@
 /*   By: luhego & parinder                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 13:11:43 by luhego & parinder #+#    #+#             */
-/*   Updated: 2023/11/22 20:31:40 by parinder         ###   ########.fr       */
+/*   Updated: 2023/11/24 19:07:17 by parinder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_builtins
+int	ft_exec_builtins(t_cmd *cmds)
+{
+	if (ft_strncmp(cmds->cmd, "echo", 5))
+		ft_echo(cmds);
+	else if (ft_strncmp(cmds->cmd, "cd", 3))
+		ft_cd(cmds);
+	else if (ft_strncmp(cmds->cmd, "pwd", 4))
+		ft_pwd(cmds);
+	else if (ft_strncmp(cmds->cmd, "export", 7))
+		ft_export(cmds);
+	else if (ft_strncmp(cmds->cmd, "unset", 6))
+		ft_unset(cmds);
+	else if (ft_strncmp(cmds->cmd, "env", 4))
+		ft_env(cmds);
+	else if (ft_strncmp(cmds->cmd, "exit", 5))
+		ft_exit(cmds);
+	else
+		return (0);
+	return (1);
+}
