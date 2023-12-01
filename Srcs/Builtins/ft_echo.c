70a4063faa69930773b9f3b6e0fb2c7e003f77f8
Patrick Rinderspacher <parinder@2G2.42angouleme.fr>
2023-12-02 00:22:10 +0100

    echo fully completed and tested, managed all the close fd in fork and out

--- Srcs/Builtins/ft_echo.c
+++ Srcs/Builtins/ft_echo.c
@@ -1,5 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luhego & parinder <marvin@42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 00:12:42 by luhego & parinder #+#    #+#             */
+/*   Updated: 2023/12/02 00:16:21 by parinder         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../minishell.h"
 
+/*
+	this function print all of its argument seperated by a single ' ' space char.
+	if the first arg is == "-n" then skip the newline char at the end.
+*/
 int	ft_echo(t_cmd *cmds)
 {
 	int	i;
@@ -9,10 +25,12 @@ int	ft_echo(t_cmd *cmds)
 		i = 2;
 	while (cmds->cmd[i])
 	{
-		write(cmds->fd_out, cmds->cmd[i], ft_strlen(cmds->cmd[i]) + 1);
+		write(cmds->fd_out, cmds->cmd[i], ft_strlen(cmds->cmd[i]));
 		i++;
+		if (cmds->cmd[i])
+			write(cmds->fd_out, " ", 1);
 	}
 	if (!cmds->cmd[1] || ft_strncmp(cmds->cmd[1], "-n", 3))
-		write(cmds->fd_out, "\n", 2);
+		write(cmds->fd_out, "\n", 1);
 	return  (0);
 }
