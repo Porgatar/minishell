ff957a9184c21c62bd567105b61633b7da880d85
Patrick Rinderspacher <parinder@student.42angouleme.fr>
2023-12-02 22:16:52 +0100

    removing redirections string from mcds->cmd in ft_redirect.c

--- Srcs/Builtins/ft_pwd.c
+++ Srcs/Builtins/ft_pwd.c
@@ -6,12 +6,15 @@
 /*   By: parinder <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 20:06:29 by parinder          #+#    #+#             */
-/*   Updated: 2023/11/24 17:03:39 by parinder         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:03:34 by parinder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+/*
+	this function print the path to working directory in fd_out.
+*/
 int	ft_pwd(t_cmd *cmd)
 {
 	char	path[1024];
