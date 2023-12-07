5862bf1acfbb8cc3d83e3adfd382de2400e8ee10
Ludovic Hego <luhego@3A7.42angouleme.fr>
2023-12-07 20:12:55 +0100

    change on ft_env/ft_unset

--- Srcs/Builtins/ft_unset.c
+++ Srcs/Builtins/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: luhego <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:58:30 by luhego            #+#    #+#             */
-/*   Updated: 2023/12/07 17:32:01 by luhego           ###   ########.fr       */
+/*   Updated: 2023/12/07 20:11:47 by luhego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 int	ft_unset(t_cmd *cmds, t_env *env)
 {
+	int		i;
 	t_env	*ptr;
 
-	ptr = ft_get_env_value(cmds->cmd[1], env);
-	if (ptr != 0)
+	i = 0;
+	while (cmds->cmd[i])
 	{
-		while (env->next != ptr)
-			env = env->next;
-		env->next = env->next->next;
-		free(ptr->key);
-		free(ptr->value);
-		free(ptr);
+		ptr = ft_get_env_value(cmds->cmd[i], env);
+		if (ptr != 0)
+		{
+			while (env->next != ptr)
+				env = env->next;
+			env->next = env->next->next;
+			free(ptr->key);
+			free(ptr->value);
+			free(ptr);
+		}
+		i++;
 	}
 	return (0);
 }
