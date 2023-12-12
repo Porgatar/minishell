26b4eebfcfd796348993f82687bba8bae8234747
Ludovic Hego <luhego@1D2.42angouleme.fr>
2023-12-12 23:53:25 +0100

    Correcting bug char -1 on cd

--- Srcs/Builtins/ft_cd.c
+++ Srcs/Builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: luhego & parinder <marvin@42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 14:59:12 by luhego & parinder #+#    #+#             */
-/*   Updated: 2023/12/07 17:24:35 by luhego           ###   ########.fr       */
+/*   Updated: 2023/12/12 23:46:26 by luhego           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,10 @@ int	ft_cd(t_cmd *cmds, t_env *env)
 	if (cmds->cmd[2])
 		printf("cd: too many arguments\n");
 	getcwd(pwd, 1024);
-	ft_strlcpy(old_pwd, pwd, ft_strlen(pwd));
+	ft_strlcpy(old_pwd, pwd, ft_strlen(pwd) + 1);
 	ft_env_new(&env, "OLDPWD", old_pwd);
 	chdir(cmds->cmd[1]);
 	getcwd(pwd, 1024);
-	/*nset(pwd);
-	getcwd(pwd, pwd[1024]);
 	ft_env_new(&env, "PWD", pwd);
-	*/return (0);
+	return (0);
 }
